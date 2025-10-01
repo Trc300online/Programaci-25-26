6ff33c7370298f9712e193b8c09dfe86b331b3f4
Toni <trieracolomar@gmail.com>
2025-10-01 10:01:40 +0200

    DONE 7-2

---
+++ JF-actT7/7-2/src/Month.java
@@ -0,0 +1,46 @@
+public class Month {
+    public int monthNumber = 5;
+
+    public void displayMonth() {
+        switch (monthNumber) {
+            case 1:
+                System.out.println("enero");
+                break;
+            case 2:
+                System.out.println("febrero");
+                break;
+            case 3:
+                System.out.println("marzo");
+                break;
+            case 4:
+                System.out.println("abril");
+                break;
+            case 5:
+                System.out.println("mayo");
+                break;
+            case 6:
+                System.out.println("junio");
+                break;
+            case 7:
+                System.out.println("julio");
+                break;
+            case 8:
+                System.out.println("agosto");
+                break;
+            case 9:
+                System.out.println("septiembre");
+                break;
+            case 10:
+                System.out.println("octubre");
+                break;
+            case 11:
+                System.out.println("noviembre");
+                break;
+            case 12:
+                System.out.println("diciembre");
+                break;
+            default:
+                System.out.println("mes no exitente!!!!");
+        }
+    }
+}
