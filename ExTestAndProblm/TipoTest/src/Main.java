74414634a2fac5109a6461fbd69770a7493e7cac
Toni <trieracolomar@gmail.com>
2025-10-03 11:55:54 +0200

    DONE first half tipo test

---
+++ ExTestAndProblm/TipoTest/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        * 1 - D
+        * 2 - C
+        * 3 - D
+        * 4 - A
+        * 5 - C
+        * 6 - B
+        * 7 - 1242.6321
+        * 8 - C
+        * 9 - ??????????
+        * 10 - A+1, A   ????
+        * 11 - C
+        * 12 - A
+        * 13 - B
+        * 14 - A
+        * 15 - A
+        * 16 - B
+        * 17 - D
+        * 18 - c = c, c = d, c = de, c = d
+        * 19 - 5 12
+        * 20 - p = false, q = true
+        *
+        * */
+    }
+}
