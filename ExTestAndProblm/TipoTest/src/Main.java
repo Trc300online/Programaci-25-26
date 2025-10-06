5e3c6192a30e7aaf3b048f9a04809a4af40d0e02
Toni <trieracolomar@gmail.com>
2025-10-06 12:16:47 +0200

    CORRECTION tipo test

--- ExTestAndProblm/TipoTest/src/Main.java
+++ ExTestAndProblm/TipoTest/src/Main.java
@@ -2,43 +2,43 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-        * 1 - D
-        * 2 - C
-        * 3 - D
-        * 4 - A
-        * 5 - C
-        * 6 - B
-        * 7 - 1242.6321
-        * 8 - C
-        * 9 - ??????????
-        * 10 - A+1, A   ????
-        * 11 - C
-        * 12 - A
-        * 13 - B
-        * 14 - A
-        * 15 - A
-        * 16 - B
-        * 17 - D
-        * 18 - c = c, c = d, c = de, c = d
-        * 19 - 5 12
-        * 20 - p = false, q = true
-        * 21 - p = ture, q = false
-        * 22 - f1
-        * 23 - ????????????????
-        * 24 - D
-        * 25 - op = 5
-        * 26 - C
-        * 27 - A
-        * 28 - x: 1
-        * 29 - ERROR
-        * 30 - B
-        * 31 - B
-        * 32 - 16
-        * 33 - A
-        * 34 - c b b a a b
-        * 35 - false false true true
-        * 36 - B
-        * 37 - A
+        * 1 - D /
+        * 2 - C /
+        * 3 - D /
+        * 4 - A /
+        * 5 - C /
+        * 6 - B X --> C
+        * 7 - 1242.6321 X --> 1242
+        * 8 - C /
+        * 9 - ?????????? X --> ERROR
+        * 10 - A+1, A   ???? X --> total=E num=D
+        * 11 - C /
+        * 12 - A /
+        * 13 - B /
+        * 14 - A X --> C
+        * 15 - A /
+        * 16 - B /
+        * 17 - D X --> A
+        * 18 - c = c, c = d, c = de, c = d /
+        * 19 - 5 12 X --> 7 12
+        * 20 - p = false, q = true /
+        * 21 - p = ture, q = false /
+        * 22 - f1 /
+        * 23 - ???????????????? X --> r=true s=false var1=2 var2=2
+        * 24 - D X --> B
+        * 25 - op = 5 /
+        * 26 - C /
+        * 27 - A /
+        * 28 - x: 1 X --> x: 2
+        * 29 - ERROR /
+        * 30 - B /
+        * 31 - B X --> A
+        * 32 - 16 /
+        * 33 - A /
+        * 34 - c b b a a b /
+        * 35 - false false true true /
+        * 36 - B X --> A
+        * 37 - A /
         * */
     }
 }
