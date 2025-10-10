d95930b6ec7c175aa4e8f9501ad5ad938659c8f7
Toni <trieracolomar@gmail.com>
2025-10-10 10:30:39 +0200

    DONE act2 & 3

--- ExProblemas/src/Main.java
+++ ExProblemas/src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         integerOverflow();
+        impSentece();
+        act3();
 
     }
 
@@ -13,5 +17,34 @@ public class Main {
             x+=x;
             System.out.println(x);
         }
+    }
+
+    public static void impSentece() {
+        String[] words = {"saber", "es", "poder"};
+        System.out.println("saber es poder");
+        System.out.println();
+        for(int i = 0; i < words.length; i++){
+            System.out.println(words[i]);
+        }
+        System.out.println("==================");
+        System.out.println("| saber es poder |");
+        System.out.println("==================");
+    }
+
+    public static void act3() {
+        Scanner real = new Scanner(System.in);
+        System.out.println("introdueix un nombre real:");
+        double nReal = real.nextDouble();
+
+        Scanner enter = new Scanner(System.in);
+        System.out.println("introdueix un nombre enter:");
+        int nEnter = enter.nextInt();
+
+        double div = nReal/nEnter;
+        System.out.println(div);
+
+        int mod = (int) div%nEnter;
+        System.out.println(mod);
+
     }
 }
