625341a463f4b8d6c0cf9e0db7d0c980a2d6808e
Trc300online <186137334+Trc300online@users.noreply.github.com>
2025-11-21 13:35:21 +0100

    DONE tipo test del punt 8

--- ExTest/TipoTest2/src/Main.java
+++ ExTest/TipoTest2/src/Main.java
@@ -1,8 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        /*
-         * 1- A
-         * */
+       /*
+        *  exercicis-imperatius-cap-3-part-1-exercicis-sentencies-seleccio.pdf
+        *
+        *  1- A
+        * 2- Positivo
+        * 3- D
+        * 4- El else corresponde al segundo if y no debe ser así. El código correcto sería:
+        *  if ( total == MAX ) {
+               if ( total < suma )
+                   System . out . println (" total es igual a MAX y total es menor que suma " );
+           } else \\ else del primer if
+               System . out . println ( " total no es igual a MAX " );
+        *
+        * 5- B
+        * 6- D
+        * 7- no, esta usando el = de asignacion y no el == de comparacion.
+        * 8- C,  Se pretendía usar la variable y fuera de su ámbito.
+        * 9- D
+        * 10- no se compara nada con 'd' en el OR, sol: comparar con char c.
+        * 11- *Dentro del bloque
+               dentroambito= 3 numero= 100
+
+               *Fuera del bloque
+                Solo existe numer 100
+        * 12- A
+        * 13-  La condicion se cumple
+               La condicion se cumple
+               La condicion se cumple
+        * 14- B
+        * 15- D
+        * 16- 1 2 3 4 5 6 7 8 9
+        * */
     }
 
     public void test() {
