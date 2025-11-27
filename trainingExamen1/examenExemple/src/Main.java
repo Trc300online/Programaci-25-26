39a61d9e997f16fed7cc2177ad515bb0a198c8a3
Trc300online <186137334+Trc300online@users.noreply.github.com>
2025-11-27 20:28:53 +0100

    CONTINUE act examen

--- trainingExamen1/examenExemple/src/Main.java
+++ trainingExamen1/examenExemple/src/Main.java
@@ -1,13 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] nums = {1, 1, 2, 3, 4, 4, 5};
+        int[] nums = {1, 3, 2, 3, 4, 4, 5};
         System.out.println(mitjana(nums));
+        System.out.println();
         System.out.println(valorMin(nums));
+        System.out.println();
         System.out.println(indexValorMin(nums));
+        System.out.println();
         System.out.println(valorMax(nums));
+        System.out.println();
         System.out.println(indexValorMax(nums));
+        System.out.println();
         System.out.println(checkSorted(nums));
+        System.out.println();
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(reverseArray(nums)));    //reverseArray(nums); also works
+        System.out.println();
+        System.out.println(searchIndex(nums, 10));
 
     }
 
@@ -77,4 +89,36 @@ public class Main {
         }
         return sorted;
     }
+
+    public static int[] reverseArray(int[] valors){
+        int[] newArray = new int[valors.length];
+        int x = 0;
+
+        for (int i = newArray.length -1; i >= 0; i--) {
+            newArray[i] = valors[x];
+            x++;
+        }
+
+        /*for (int i = 0; i < newArray.length; i++){
+            System.out.print(newArray[i] + ", ");
+        }*/ // se usa si no se usa Arrays.toString i return de int[]
+
+        return newArray;
+    }
+
+    public static int searchIndex(int[] valors, int looking) {
+        int index = -1;
+
+        for (int i = 0; i < valors.length; i++) {
+            if (valors[i] == looking) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    // TODO: act 10, act 11, act 12.
+
 }
