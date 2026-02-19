06bb04cb5936c6973cc22d1956e8cc8392690490
Trc300online <186137334+Trc300online@users.noreply.github.com>
2026-02-19 23:02:03 +0100

    DONE 5-3

--- JP-actT5/5-3/src/EnumBanking/src/com/example/EnumBankingMain.java
+++ JP-actT5/5-3/src/EnumBanking/src/com/example/EnumBankingMain.java
@@ -24,13 +24,11 @@ public class EnumBankingMain {
         Customer customer;
         
         //180 day term
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_YEAR, 180);
-        
+
         // Create several customers and their accounts
         bank.addCustomer("Jane", "Simms");
         customer = bank.getCustomer(0);
-        customer.addAccount(new TimeDepositAccount(500.00, cal.getTime()));
+        customer.addAccount(new TimeDepositAccount(500.00, DepositLength.SIX_MONTHS));
         customer.addAccount(new CheckingAccount(200.00, 400.00));
 
         bank.addCustomer("Owen", "Bryant");
@@ -39,13 +37,13 @@ public class EnumBankingMain {
 
         bank.addCustomer("Tim", "Soley");
         customer = bank.getCustomer(2);
-        customer.addAccount(new TimeDepositAccount(1500.00, cal.getTime()));
+        customer.addAccount(new TimeDepositAccount(1500.00, DepositLength.THREE_MONTHS));
         customer.addAccount(new CheckingAccount(200.00));
 
         bank.addCustomer("Maria", "Soley");
         customer = bank.getCustomer(3);
         // Maria and Tim have a shared checking account
         customer.addAccount(bank.getCustomer(2).getAccount(1));
-        customer.addAccount(new TimeDepositAccount(150.00, cal.getTime()));
+        customer.addAccount(new TimeDepositAccount(150.00, DepositLength.SIX_MONTHS));
     }
 }
